95d6ab63440a65605497d743392c6f70a81f4abb
Patrick Coxall <mr.coxall@mths.ca>
2021-06-29 17:04:39 +0000

    fix indenting to 2 spaces

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,18 +7,18 @@
 * @since   2020-09-01
 */
 final class HelloWorld {
-    private HelloWorld() {
-        // Prevent instantiation
-        // Optional: throw an exception e.g. AssertionError
-        // if this ever *is* called
-        throw new IllegalStateException("Cannot be instantiated");
-    }
-    /**
-    * This method prints out "Hello, World!".
-    * @param args
-    */
-    public static void main(final String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("\nDone.");
-    }
+  private HelloWorld() {
+    // Prevent instantiation
+    // Optional: throw an exception e.g. AssertionError
+    // if this ever *is* called
+    throw new IllegalStateException("Cannot be instantiated");
+  }
+  /**
+  * This method prints out "Hello, World!".
+  * @param args
+  */
+  public static void main(final String[] args) {
+    System.out.println("Hello, World!");
+    System.out.println("\nDone.");
+  }
 }
